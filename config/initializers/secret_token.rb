4b4efa91762566b5166b7e058f1988046d6ee68d
John Meehan <john.f.meehan@gmail.com>
2013-11-26 17:10:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pintestring::Application.config.secret_key_base = 'f52330d5073cc77ac9b0be8a785d17f81ec9a3bec6ab7daece71f1e6a60d5328d9829a23e55148bc1489e1710c5e17cbc909e61e42aed1f5b7029b13f58986fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pintestring::Application.config.session_store :cookie_store, key: '_pintestring_session'
